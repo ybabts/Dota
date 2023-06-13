9f6a7101f58eb592b6162415236f1f70d91082ec
Tyler Mueller <muelltyl@gmail.com>
2023-06-12 20:59:19 -0500

    todo messages

--- src/api/mod.ts
+++ src/api/mod.ts
@@ -17,3 +17,9 @@ export function setAPIKey(key: apiKey): void {
 export function setEndpoint(url: string): void {
   endpoint = url;
 }
+
+// TODO(ybabts): implement https://www.dota2.com/webapi/IDOTA2League/GetLeaguesData/v001?league_ids=15350,15353,15365,15366,15351,15352,15335,15336,15383,15384,15374,15375,&delay_seconds=0
+// TODO(ybabts): implement https://www.dota2.com/react/getlivestreams?league_ids=15350,15353,15365,15366,15351,15352,15335,15336,15383,15384,15374,15375
+// TODO(ybabts): implement https://store.steampowered.com/events/ajaxgetpartnereventspageable/?clan_accountid=0&appid=570&offset=0&count=100&l=english&origin=https:%2F%2Fwww.dota2.com
+// TODO(ybabts): change to Errors as Values
+// TODO(ybabts): refactor naming scheme
